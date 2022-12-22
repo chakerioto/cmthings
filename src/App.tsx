6371cf3b1d6a28cd0c042da1f8035a961b9d8495
Thang Quoc Nguyen <quocthang.nguyen@savvycomsoftware.com>
2022-12-22 15:49:56 +0700

    paralax scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="onehundredvh"></div>
+      <div className="parallax">
+        <div className="">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore,
+          eveniet. Et atque assumenda placeat velit eaque necessitatibus, enim
+          illum molestias vel maxime saepe, eos repellat cum quisquam inventore.
+          Dolores, error. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Inventore, eveniet. Et atque assumenda placeat velit eaque
+          necessitatibus, enim illum molestias vel maxime saepe, eos repellat
+          cum quisquam inventore. Dolores, error. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Inventore, eveniet. Et atque assumenda
+          placeat velit eaque necessitatibus, enim illum molestias vel maxime
+          saepe, eos repellat cum quisquam inventore. Dolores, error. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Inventore, eveniet.
+          Et atque assumenda placeat velit eaque necessitatibus, enim illum
+          molestias vel maxime saepe, eos repellat cum quisquam inventore.
+          Dolores, error.
+        </div>
+      </div>
+      <div className="onehundredvh"></div>
     </div>
   );
 }
